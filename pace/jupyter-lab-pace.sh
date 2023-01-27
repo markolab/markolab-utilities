a26598a9ef92a0b6727e9749d6f2c452bd43ec50
Jeffrey E Markowitz <jmarkowitz30@atl1-1-03-004-2-r.pace.gatech.edu>
2023-01-27 00:22:23 -0500

    Minor fix

--- pace/jupyter-lab-pace.sh
+++ pace/jupyter-lab-pace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$JUPYTER_PORT" ]; then 
-	port=$(shuf -i 20000-30000 -n 1)
+	port=$(shuf -i 5000-8000 -n 1)
 else
 	port=$JUPYTER_PORT
 fi
@@ -9,7 +9,7 @@ fi
 hname=$(hostname)
 
 echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
-echo -e "\nSSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hname)"
+echo -e "\nSSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@${hname}"
 
 cd ${HOME}
 jupyter lab --no-browser --port=${port} --ip=0.0.0.0 --NotebookApp.custom_display_url="http://$hname:$port"
