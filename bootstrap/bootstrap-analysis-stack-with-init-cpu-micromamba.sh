b939d85151ac8b6225e0b0f99f7dd052be62954d
Jeff Markowitz <jeffrey.markowitz@bme.gatech.edu>
2023-06-19 12:50:24 -0400

    Minor changes

--- bootstrap/bootstrap-analysis-stack-with-init-cpu-micromamba.sh
+++ bootstrap/bootstrap-analysis-stack-with-init-cpu-micromamba.sh
@@ -6,8 +6,9 @@ curl "${ENV_URL}/base.yml" -o ~/base.yml -s
 curl "${ENV_URL}/datastack.yml" -o ~/datastack.yml -s
 curl "${ENV_URL}/segmentation-cpu.yml" -o ~/segmentation-cpu.yml -s
 
+sudo curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj /bin/micromamba
 export MAMBA_ROOT_PREFIX=~/micromamba  # optional, defaults to ~/micromamba
-eval "$(./bin/micromamba shell hook -s posix)" # now we should be good...
+eval "$(/bin/micromamba shell hook -s posix)" # now we should be good...
 
 micromamba activate
 micromamba install -f ~/base.yml -y
