97b8077a42e17a57e5af5695fd91c419dabc8d15
Jeffrey Markowitz <markowitzmeister@gmail.com>
2023-06-24 21:13:05 -0400

    Added cuda version checks

--- bootstrap/bootstrap-analysis-stack-with-init-cuda.sh
+++ bootstrap/bootstrap-analysis-stack-with-init-cuda.sh
@@ -7,6 +7,13 @@ else
 	INSTALLDIR=${HOME}/miniconda3
 fi
 
+if [ -z ${CUDA_VERSION_MAJOR} ]; then
+	CUDA_VERSION_VERBOSE=$(cat /usr/local/cuda/version.json | jq -r '.["cuda"].version')
+	CUDA_VERSION_MAJOR=$(echo ${CUDA_VERSION_VERBOSE} | cut -d. -f1)
+	CUDA_VERSION_MINOR=$(echo ${CUDA_VERSION_VERBOSE} | cut -d. -f2)
+	CUDA_VERSION=${CUDA_VERSION_MAJOR}.${CUDA_VERSION_MINOR}
+fi
+
 # get miniconda and install
 UNAME_M="$(uname -m)" 
 OS="$(uname)"
@@ -28,10 +35,10 @@ bash ~/miniconda.sh -b -p $INSTALLDIR
 ENV_URL="https://raw.githubusercontent.com/markolab/markolab-envs/main"
 curl "${ENV_URL}/base.yml" -o ~/base.yml -s
 curl "${ENV_URL}/datastack.yml" -o ~/datastack.yml -s
-curl "${ENV_URL}/segmentation-cuda-12_1.yml" -o ~/segmentation-cuda-12_1.yml -s
+curl "${ENV_URL}/segmentation-cuda-${CUDA_VERSION_MAJOR}_${CUDA_VERSION_MINOR}.yml" -o ~/segmentation-cuda-${CUDA_VERSION_MAJOR}_${CUDA_VERSION_MINOR}.yml -s
 
 source ${INSTALLDIR}/bin/activate
 conda env update --file ~/base.yml --prune
 conda env create -f ~/datastack.yml
-conda env create -f ~/segmentation-cuda-12_1.yml
+conda env create -f ~/segmentation-cuda-${CUDA_VERSION_MAJOR}_${CUDA_VERSION_MINOR}.yml
 conda init bash
