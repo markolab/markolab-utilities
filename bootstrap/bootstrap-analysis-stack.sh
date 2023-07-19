eef1caf3d5834d39ed32667a66fa22b2b8b4ae01
Jeffrey Markowitz <markowitzmeister@gmail.com>
2023-07-19 13:39:48 -0400

    Update bootstrap-analysis-stack.sh

--- bootstrap/bootstrap-analysis-stack.sh
+++ bootstrap/bootstrap-analysis-stack.sh
@@ -28,10 +28,13 @@ bash ~/miniconda.sh -b -p $INSTALLDIR
 ENV_URL="https://raw.githubusercontent.com/markolab/markolab-envs/main"
 curl "${ENV_URL}/base.yml" -o ~/base.yml -s
 curl "${ENV_URL}/datastack.yml" -o ~/datastack.yml -s
+curl "${ENV_URL}/segmentation-cpu.yml" -o ~/segmentation-cpu.yml -s
 
 source ${INSTALLDIR}/bin/activate
 conda env update --file ~/base.yml --prune
 conda env create -f ~/datastack.yml
+conda env create -f ~/segmentation-cpu.yml
+
 # DO NOT ALTER BASHRC
 # conda init bash 
 
